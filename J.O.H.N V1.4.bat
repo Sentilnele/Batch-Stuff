101b6e720d65e29c1d2fc3bdd74694f3f156fa12
Sentilnele <52940910+Sentilnele@users.noreply.github.com>
2019-07-15 21:40:26 -0300

    Add files via upload

---
+++ J.O.H.N V1.4.bat
@@ -0,0 +1,634 @@
+@echo off
+color C
+title J.O.H.N
+echo J.O.H.N RAT HAS BEEN ACTIVATED, YOUR PASSWORDS ARE COMPROMISED                                                                                          
+echo start https://www.pornhub.com > dont_run.bat   
+echo jasha > J.O.H.N.vbs
+echo hasd > J.0.H.N.tmp
+echo hlsdja > J.0.H.n.dll
+echo asdjao > j.0.H.N.scpt
+echo asdjask > J.o.H.N.js        
+echo         +s:              `-+syyyso:.              /s/        :+            .oso.       -s+        
+echo         mMs            `smNho+/+ohNNy.            hMd        sMN            oMMMN:      oMm        
+echo         mMs           .dMd-       -dMm.           hMd        sMN            oMm+NN/     oMm        
+echo         mMs           yMN`         .NMy           hMd        sMN            oMN /NMo    oMm        
+echo         mMs           NMh           dMm           hMNyyyyyyyymMN            oMN  :mMs`  oMm        
+echo         mMs           MMy           dMm           hMm++++++++dMN            oMN   -mMy` oMm        
+echo         mMs           dMm`         .NMs           hMd        sMN            oMN    .dMd.oMm        
+echo         NMs     ``    :NMs`       .hMm.    ``     hMd        sMN     `.     oMN     `yMdyMm        
+echo    -o::oMM:    smm-    /dMms/:::+yNNy.    /mm+    hMd        sMN    .mmy    oMN      `sMMMm        
+echo    .yhdhy:     /hy.     `:oyhddhhs/.      -yh:    oho        /hy    `yh+    :hs        /yho        
+echo. 
+echo Press enter to activate removal.
+if exist "Whitelist.txt" goto formal exit                      
+pause >nul
+
+set /p AutoRemover=Enter code to not send:
+
+if %AutoRemover%==6674 goto ehega
+if %AutoRemover%==3301 goto Removal
+if not %AutoRemover%==3301 goto Wrong
+pause >nul
+pause >nul
+pause >nul
+pause >nul
+pause >nul
+pause >nul
+pause >nul
+pause >nul
+pause >nul
+
+
+
+
+
+
+
+
+
+
+:Wrong
+echo Code is wrong, tracing location...
+ping localhost -n 4 >nul
+ipconfig
+ping localhost -n 4 >nul
+echo Thanks for your location, ill come there soon.
+ping localhost -n 4 >nul
+cls
+echo Closing...
+ping localhost -n 5 >nul
+exit
+
+
+
+
+:Removal
+@echo off
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+del j.0.H.N.scpt
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+del J.0.H.n.dll
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+del J.0.H.N.tmp
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+del J.o.H.N.js
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+del J.O.H.N.vbs
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+echo Removing /
+ping localhost -n 1.5 >nul
+cls
+echo rEmoving -
+ping localhost -n 1.5 >nul
+cls
+echo reMoving \
+ping localhost -n 1.5 >nul
+cls
+echo remOving -
+ping localhost -n 1.5 >nul
+cls
+echo remoVing /
+ping localhost -n 1.5 >nul
+cls
+echo removIng -
+ping localhost -n 1.5 >nul
+cls
+echo removiNg \
+ping localhost -n 1.5 >nul
+cls
+echo removinG -
+ping localhost -n 1.5 >nul
+cls
+
+
+
+del dont_run.bat
+cls
+echo Done removing, closing.
+ping localhost -n 5 >nul
+goto :exit
+
+
+:exit
+exit
+
+
+
+:formal exit
+cls
+echo Whitelist detected, cancelling infection...
+del J.O.H.N.vbs
+del J.0.H.N.tmp
+del J.0.H.n.dll
+del j.0.H.N.scpt
+del J.o.H.N.js
+ping localhost -n 3 >nul
+cls
+echo Done, exiting...
+ping localhost -n 3 >nul
+goto exit
+
+
+
+
+
+
+:ehega
+cls
+del J.O.H.N.vbs
+del J.0.H.N.tmp
+del J.0.H.n.dll
+del j.0.H.N.scpt
+del J.o.H.N.js 
+echo This pc has been whitelisted...
+echo doodle > Whitelist.txt
+ping localhost -n 4 >nul
