aa0f2323a9e77d688f821a7e916ba88caa90e9e9
Sentilnele <52940910+Sentilnele@users.noreply.github.com>
2019-07-17 03:05:09 -0300

    Add files via upload

---
+++ Lazy-Key-System.bat
@@ -0,0 +1,69 @@
+@echo off
+color C
+title Key Giver
+set /P input=Code:
+if %input%==3301 echo -!72272!- is the key
+if %input%==3301 echo.
+if %input%==3301 echo.
+if %input%==3301 echo  `.--::--.              `.......``                                                           
+if %input%==3301 echo       -++++//////-        `-:////////////:-`                                                       
+if %input%==3301 echo      .++ossyyys+//:     `-//////////////////:.                                                     
+if %input%==3301 echo      -++o/:--/+o//+`  `-//////+ooossso+///////-`                                                   
+if %input%==3301 echo      -o/+:.```.++++-.`/+///+osso++//osys+//////:`                                                  
+if %input%==3301 echo      `+o+////+oso+////+////+/-.``````.-+y+//////:`                                                 
+if %input%==3301 echo       -osooosso++//oo/++//+-``       ``.:oo//////.                                                 
+if %input%==3301 echo        `-+ososo+ooo+//s///-`           ``.++///::-                                                 
+if %input%==3301 echo          ``--/o/+++++oo///.              `:+/////-                                                 
+if %input%==3301 echo         `.//////++++++////.              `://////.                                                 
+if %input%==3301 echo        `://++/++ossssoo++/`             `-+/////:                                                  
+if %input%==3301 echo       `:////+ooo+//++oso+.            `.:///////`                                                  
+if %input%==3301 echo      `:////++:.```````.``  `::-...--::/++////+/`                                                   
+if %input%==3301 echo      -////+:.              ://////////////+++:`                                                    
+if %input%==3301 echo      //////`               :+//o+++++++++/+/.                                                      
+if %input%==3301 echo      /////:                .++/s///+oo+/////`                                                      
+if %input%==3301 echo      ://///`              `/++/s/+o+//////+/-.                                                     
+if %input%==3301 echo      .+/////.`          `./++++s+s+///ooo+////.                                                    
+if %input%==3301 echo       :+/////:-``    ``-/+//++s+/+/+oo//////:/:                                                    
+if %input%==3301 echo        /o+//////:::///+////+++o++//s+/////////:`                                                   
+if %input%==3301 echo        `:oo+///////////////++ohsooo///////////::-`                                                 
+if %input%==3301 echo          .:oo++/////////++oo/.-/++so++++///://///:.                                                
+if %input%==3301 echo            `-/osssooosoo+:.```````:+oooss+/////:///:.                                              
+if %input%==3301 echo                `....```            ````.+oo+/////:///-`                                            
+if %input%==3301 echo                                         `./so+/////////-                                           
+if %input%==3301 echo                                           `:ooo//////////.                                         
+if %input%==3301 echo                                             `:oo+/////////:`                                       
+if %input%==3301 echo                                               ./oo+/////////-`                                     
+if %input%==3301 echo                                                `.+oo+//://////-`                                   
+if %input%==3301 echo                                                  `-ooo/////////:.                 ``               
+if %input%==3301 echo                                                    `:oo+//:::////:`             `-:/-`             
+if %input%==3301 echo                                                      `/oo//::://///-`         .:////:`             
+if %input%==3301 echo                                                        -/o+/:::::///:-`    `.-//////-              
+if %input%==3301 echo                                                         `-+o+/::::////:.``.://::////-              
+if %input%==3301 echo                                                           `:+o+/::/+o+//::///://://:-.```   `      
+if %input%==3301 echo                                                             ./oo/:/++o+//:/+////////:::::..::.     
+if %input%==3301 echo                                                               .+o+/+///////+++//:/://///::///-`    
+if %input%==3301 echo                                                                `-+oo/:///:://++//::///+////:.`     
+if %input%==3301 echo                                                                  `:oo+///////:/++/:///++/:.        
+if %input%==3301 echo                                                                    ./oo///////::/+/:` ```          
+if %input%==3301 echo                                                                      ./oo//////::://.              
+if %input%==3301 echo                                                                       `-+o+/////:///:.             
+if %input%==3301 echo                                                                         `-+o+///++++::`            
+if %input%==3301 echo                                                                           `:oo//+o+//:`            
+if %input%==3301 echo                                                                             `:s++///:`             
+if %input%==3301 echo                                                                               .://:-`       You have 30 seconds to copy it, or view it... Scroll up.            
+if %input%==3301 echo.                                                                                                    
+if %input%==3301 echo.                                                                                     
+if not %input%==3301 echo Wrong code...
+ping localhost -n 30 >nul
+cls
+echo Exiting in 3
+ping localhost -n 2 >nul
+cls
+echo Exiting in 2
+ping localhost -n 2 >nul
+cls
+echo Exiting in 1
+ping localhost -n 2 >nul 
+echo Exiting...
+ping localhost -n 2 >nul
+exit
